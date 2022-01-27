567bced49ada5be0c718de74b1917c15cc847e45
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2022-01-27 12:03:10 -0800

    Add JUnit tests for each test file using the getLinks method

--- markdown-parse/MarkdownParseTest.java
+++ markdown-parse/MarkdownParseTest.java
@@ -1,9 +1,48 @@
 import static org.junit.Assert.*; //allows you to use Assert methods in your code
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParseTest { //class header
     @Test //tells JUnit that this method is a test method
     public void addition() { //method header
         assertEquals(2, 1 + 1);//checks that 2 is equal to 1 + 1
+    }
+
+    @Test
+    public void getLinks1() throws IOException{
+        // checks test-file.md
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), expected);
+    }
+
+    @Test
+    public void getLinks2() throws IOException{
+        // checks new-test.md
+        List<String> expected = List.of("image.jpg");
+        Path fileName = Path.of("new-test.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), expected);
+    }
+
+    @Test
+    public void getLinks3() throws IOException{
+        List<String> expected = List.of("google.com");
+        Path fileName = Path.of("test-file3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), expected);
+    }
+
+    @Test
+    public void getLinks4() throws IOException{
+        List<String> expected = List.of();
+        Path fileName = Path.of("test-file4.md");
+        String contents = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contents), expected);
     }
 }
