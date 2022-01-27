0b3e1dac373b1cd705b61221a32851601c783763
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2022-01-27 11:47:15 -0800

    Create MarkdownParseTest.java
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

---
+++ markdown-parse/MarkdownParseTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*; //allows you to use Assert methods in your code
+import org.junit.*;
+
+public class MarkdownParseTest { //class header
+    @Test //tells JUnit that this method is a test method
+    public void addition() { //method header
+        assertEquals(2, 1 + 1);//checks that 2 is equal to 1 + 1
+    }
+}
